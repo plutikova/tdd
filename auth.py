0ca2c8e17dbc39673bfc6e30887ce34417106c4c
plutikova <lina.plutikova@mail.ru>
2018-12-06 19:30:49 +0300

    commit_1

---
+++ auth.py
@@ -0,0 +1,51 @@
+import hashlib
+from typing import Optional, Tuple
+
+
+class Auth:
+    def __init__(self, filename: str):
+        self._filename = filename
+        self._is_users_changed = False
+
+        self._users = []
+
+        with open(self._filename, 'r') as f:
+            for line in f:
+                name, pw_hash = line.split(',')
+                self._users.append((name, pw_hash))
+
+    @property
+    def users_amount(self) -> int:
+        return len(self._users)
+
+    def has_user(self, username: str) -> bool:
+        user = self._get_user(username)
+        return user is not None
+
+    def validate_user(self, name: str, password: str) -> bool:
+        user = self._get_user(name)
+
+        if user is not None:
+            (username, pw_hash) = user
+            _hash = hashlib.md5(password.encode()).hexdigest()
+            return username == name and pw_hash == _hash 
+        else:
+            return False
+
+    def add_user(self, name: str, password: str):
+        pw = hashlib.md5(password.encode()).hexdigest()
+        line = ','.join([name, pw])
+
+        with open(self._filename, 'a') as f:
+            f.write('\n' + line)
+
+        self._users.append((name, pw))
+    
+
+
+    def _get_user(self, username: str) -> Optional[Tuple[str, str]]:
+        for (name, pw_hash) in self._users:
+            if name == username:
+                return (name, pw_hash)
+        
+        return None
---
+++ tests.py
@@ -0,0 +1,47 @@
+import unittest
+
+from auth import Auth
+
+
+class TestAuth(unittest.TestCase):
+    def setUp(self):
+        self._file_name = 'test_users.txt'
+
+        self.auth = Auth(self._file_name)
+
+        self._old_file_content = ''
+
+        with open(self._file_name, 'r') as f:
+            self._old_file_content = f.read()
+
+    def tearDown(self):
+        with open(self._file_name, 'w') as f:
+            f.write(self._old_file_content)
+
+    def test_has_user(self):
+        result = self.auth.has_user('admin')
+        self.assertTrue(result)
+        result2 = self.auth.has_user('user')
+        self.assertFalse(result2)
+
+    def test_users_amount(self):
+        self.assertEqual(self.auth.users_amount, 1)
+
+    def test_validate_user(self):
+        is_valid = self.auth.validate_user(name='admin', password='0000')
+        self.assertTrue(is_valid)
+        is_valid2 = self.auth.validate_user(name='admin', password='1111')
+        self.assertFalse(is_valid2)
+        is_valid3 = self.auth.validate_user(name='admin1', password='0000')
+        self.assertFalse(is_valid3)
+
+    def test_add_user(self):
+        user_name = 'user'
+        self.auth.add_user(name=user_name, password='1111')
+        result = self.auth.has_user(user_name)
+        self.assertTrue(result)
+        self.assertEqual(self.auth.users_amount, 2)
+    
+
+if __name__ == '__main__':
+    unittest.main()
